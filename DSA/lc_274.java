a3c6297278c5d4ac5ad5491c596642a36a557e84
apranshyadav <apranshy@gmail.com>
2025-08-15 21:45:51 +0530

    krdeee

---
+++ DSA/lc_274.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int paper = citations.length;
+        int[] cites = new int[paper + 1];
+        for(int i: citations) cites[Math.min(i, paper)]++;
+        int total = 0;
+        for(int i = paper; i >= 0; i--) {
+            total += cites[i];
+            if(total >= i) return i;
+        }
+        return 0;
+    }
+}
