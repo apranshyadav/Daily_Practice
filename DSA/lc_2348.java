5e2432212434736cba54fdc7f1a90df0b2c251d9
apranshyadav <apranshy@gmail.com>
2025-08-19 21:07:42 +0530

    DOOO itt

---
+++ DSA/lc_2348.java
@@ -0,0 +1,10 @@
+public class lc_2348 {
+    public long zeroFilledSubarray(int[] nums) {
+        long cnt = 0, streak = 0;
+        for (int num : nums) {
+            streak = (num == 0) ? streak + 1 : 0;
+            cnt += streak;
+        }
+        return cnt;
+    }
+}
