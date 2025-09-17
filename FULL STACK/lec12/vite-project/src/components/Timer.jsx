7e834958c47727c699aee3d804d7adbacb7728c9
apranshyadav <apranshy@gmail.com>
2025-09-17 15:44:22 +0530

    bahut saara

--- FULL STACK/lec12/vite-project/src/components/Timer.jsx
+++ FULL STACK/lec12/vite-project/src/components/Timer.jsx
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from "react";
 
+
+// LEGALLY WAY
+// const Timer = () => {
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCount((prev) => prev + 1);
+//     }, 1000);
+
+//     // Cleanup function which executes between two re-renders
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Timer: {count} seconds</h1>
+//     </div>
+//   );
+// };
+
+
+//JUGAAD
 const Timer = () => {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCount((prev) => prev + 1);
+    setTimeout(() => {
+      setCount(count+1);
     }, 1000);
-
-    // Cleanup function which executes between two re-renders
-    return () => clearInterval(interval);
-  }, []);
+  }, [count]);
 
   return (
     <div>
