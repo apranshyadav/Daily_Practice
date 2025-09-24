92a1a7659e4909ea6a448547e6e79ae206625838
apranshyadav <apranshy@gmail.com>
2025-09-24 14:43:23 +0530

    bahut saara

---
+++ FULL STACK/lec13/vite-project/components/Person.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Person = () => {
+    const [dog, setDog] = useState("Simba");
+
+    // Function to change dog
+    function changeDog() {
+        setDog(Math.floor(Math.random() * 10)); 
+    }
+
+    // Child component
+    function Chotu({ naam }) {
+        return <h1>Chai leke aaya malik: {naam}</h1>;
+    }
+
+    return (
+        <div>
+            
+            <Chotu naam={dog} />
+            <button onClick={changeDog}>Change Dog</button>
+            <Chotu naam="Chotu" />
+            <Chotu naam="Simba" />
+            <Chotu naam="Nepali" />
+            <Chotu naam="Dodo" />
+        </div>
+    );
+};
+
+export default Person;
